9c7907b7b391442ee69e9b2fc6a679d49e4fc55e
gjones.pf@gmail.com <gjones.pf@gmail.com>
2016-10-31 18:20:58 +1300

    Add some sleepy time, due to intermittent issues

--- init.d/chown.sh
+++ init.d/chown.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 chown www-data:www-data /app -R &
 
+#Issues with IO sometimes too slow on aufs, which causes "Text file busy" downstream due to chown here...
+#https://github.com/docker/docker/issues/9547
+sync; sleep 1;
