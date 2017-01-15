4fc08294e2cfb412ad76a237295ff34075a44b2a
gjones.pf@gmail.com <gjones.pf@gmail.com>
2017-01-16 08:38:06 +1300

    Env related tweaks

--- run.sh
+++ run.sh
@@ -8,10 +8,13 @@ do
     if [ -f $SCRIPT -a -x $SCRIPT ]
     then
         echo "Executing startup script $SCRIPT"
+        sync; sleep 1;
         $SCRIPT
     fi
 done
 
 source /etc/apache2/envvars
+touch /scripts/.apacheenv
+source /scripts/.apacheenv
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
